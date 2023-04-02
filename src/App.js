1589e4cc1ac3fdad2b82acf429bcbea89c3163ba
tdanielles <trisha.d.sia@gmail.com>
2023-04-02 12:24:32 -0700

    most stuff done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Demo from './pages/Demo';
 import Landing from './pages/Landing';
+import Transition from './pages/Transition';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing/>}/>
+        <Route path="/transition" element={<Transition/>}/>
         <Route path="/demo" element={<Demo/>}/>
       </Routes>
     </>
