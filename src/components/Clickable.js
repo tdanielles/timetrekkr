96bd9b391094c418b6220873ba58a3aa505fd664
tdanielles <trisha.d.sia@gmail.com>
2023-04-01 23:19:03 -0700

    ai and text to speech stuff works

--- src/components/Clickable.js
+++ src/components/Clickable.js
@@ -1,6 +1,5 @@
 import '../styles/Clickable.css';
 import { useState } from 'react';
-import { Configuration, OpenAIApi } from 'openai';
 
 function Clickable(props) {
     const { imgUrl, desc, learnmore, isOneClicked, setRestriction } = props;
@@ -8,7 +7,6 @@ function Clickable(props) {
 
     // OpenAI stuff
     const [apiResponse, setApiResponse] = useState("");
-    const API_KEY = "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";
 
     const displayInfoBox = (e) => {
         if (!isOneClicked) {
@@ -21,10 +19,11 @@ function Clickable(props) {
         setClicked(false);
         setRestriction(false);
         setApiResponse("");
+        window.speechSynthesis.cancel();
     }
 
     const generateStuff = async (e) => {
-        let prompt = e.target.innerHTML;
+        /*let prompt = e.target.innerHTML;
         const APIBody = {
             "model": "text-davinci-003",
             "prompt": "You're teaching history to a 6-year-old. I will provide a prompt about a historical statement. You will tell me more about the historical statement using an elementary educational approach. This is the prompt: " + prompt,
@@ -47,7 +46,7 @@ function Clickable(props) {
         }).then((data) => {
             console.log(data.choices[0].text.trim());
             setApiResponse(data.choices[0].text.trim());
-        });
+        });*/
         
         
     }
@@ -56,17 +55,37 @@ function Clickable(props) {
         setApiResponse("");
     }
 
+    // TEXT TO SPEECH STUFF
+    const msg = new SpeechSynthesisUtterance();
+    
+
+    const speechHandler = (msg) => {
+        if (apiResponse !== "") {
+            msg.text = apiResponse;
+        } else {
+            msg.text = desc;
+        }
+        let voices = window.speechSynthesis.getVoices();
+        msg.voice = voices[7]; // 0 (boring guy), 7 (british guy), 10 (lady but with little bit of accent), 11 (old guy), 17 (british lady), 28 (lady), 33 (lady), 37, 49-51 good but cuts off
+        msg.pitch = 2;
+        window.speechSynthesis.speak(msg);
+    }
+
+
     return(
         <div className="Clickable">
             <img className="clickable-img" src={imgUrl} onClick={displayInfoBox}/>
             {clicked && <div className="info-box">
                 {apiResponse !== "" ?  apiResponse : desc}
+                <p></p>
+                <p></p>
                 {learnmore.map((learn) => (
                     <p>Learn more about <span className="emphasis" onClick={generateStuff}>{learn}</span></p>
                 ))}
                 <div className='buttons'>
                     <button onClick={backToOriginal}>Back to Original Text</button>
                     <button onClick={hideInfoBox}>Exit</button>
+                    <button onClick={() => speechHandler(msg)}>Read To Me</button>
                 </div>
             </div>}
         </div>
